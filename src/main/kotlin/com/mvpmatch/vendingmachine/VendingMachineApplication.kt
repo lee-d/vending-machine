ce858e613930bdebc216834b924cac465a0f1bc2
Daniel Lee <info@lee-d.com>
2022-01-31 22:08:44 +0100

    Initial commit

---
+++ src/main/kotlin/com/mvpmatch/vendingmachine/VendingMachineApplication.kt
@@ -0,0 +1,11 @@
+package com.mvpmatch.vendingmachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VendingMachineApplication
+
+fun main(args: Array<String>) {
+	runApplication<VendingMachineApplication>(*args)
+}
